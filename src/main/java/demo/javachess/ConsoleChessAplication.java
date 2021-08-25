45856744143d8cd6a135a672eaa3433dbeb27ffc
최창준 <aowert6518@gmail.com>
2021-08-25 18:20:44 +0900

    [style] 코드 전체 초기화 - 처음부터 다시 시작

--- src/main/java/demo/javachess/ConsoleChessAplication.java
+++ src/main/java/demo/javachess/ConsoleChessAplication.java
@@ -1,10 +1,7 @@
 package demo.javachess;
 
-import demo.javachess.controller.ChessGame;
 
 public class ConsoleChessAplication {
     public static void main(String[] args) {
-        ChessGame chessGame = new ChessGame();
-        chessGame.run();
     }
 }
